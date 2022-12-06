a8a9cb80d3668c2d4f39901488ad950404dca5a6
JamesWalker2008 <113200335+JamesWalker2008@users.noreply.github.com>
2022-12-06 10:38:28 -0800

    poopoo

--- Prototype 3/Assets/Course Library/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Course Library/Scripts/PlayerController.cs
@@ -15,9 +15,8 @@ public class PlayerController : MonoBehaviour
     {
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
-        playerRb = GetComponent<Rigidbody>();
-       
-      }
+        
+    }
 
     // Update is called once per frame
     void Update()
@@ -26,7 +25,7 @@ public class PlayerController : MonoBehaviour
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isonGround = false;
-            
+            playerRb.AddForce(Vector3.up * 100, ForceMode Impulse); 
         }
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
